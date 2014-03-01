5e1f39d7bb41193f79795088e95cb20c343d1006
Rajik <krajeswaribtech@gmail.com>
2014-03-01 12:10:46 +0530

    Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseApi::Application.config.secret_key_base = '917ee1f32c80ea658ff913ca6993ab93eb0f92b34db976e01aa84dcd1cb844e6ec71dba619f11bbefa75cec9a3e27a2ddcd306ba9fa96bff2a847d8206980ac2'
